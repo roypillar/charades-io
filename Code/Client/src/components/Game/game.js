95feadfbc5ea7318a1abef48a85a01469899e1ad
Omri Basch <omrib15@gmail.com>
2019-11-08 14:22:49 +0200

    addu tility module- io.js for handling game component communication with server

--- Code/Client/src/components/Game/game.js
+++ Code/Client/src/components/Game/game.js
@@ -3,15 +3,18 @@ import {Redirect} from 'react-router-dom';
 import Teams from './Teams'
 import {SOCKET_IO_SERVER_URL} from '../../remote/addresses';
 import io from "socket.io-client";
+import {setUpSocketHandlers} from "./util/io.js";
 
 class Game extends Component {
 
     constructor(props) {
         super(props);
         console.log(this.props);
+        const socket = io(SOCKET_IO_SERVER_URL);
+        
         if (this.props.location.state) //if we have been redirected (normal control flow)
             this.state = {
-                socket: null,
+                socket: socket,
                 roomId: this.props.match.params.id,
                 userName: this.props.location.state.userName,
                 gameName: this.props.location.state.gameName,
@@ -64,58 +67,15 @@ class Game extends Component {
     componentDidMount() {
         if(this.state.redirect)
             return;
-
-        const userName = this.state.userName;
-        let roomId = this.state.roomId;
-        const cameFrom = this.state.cameFrom;
-        const gameName = this.state.gameName;
-
-        const socket = io(SOCKET_IO_SERVER_URL);
-        this.setState({socket: socket});
         
+        setUpSocketHandlers(this);
 
-        socket.on('connect', function () {
-            console.log('camefrom',cameFrom);
-            
-            if(cameFrom === 'create'){
-                socket.emit('create_room', userName, gameName);
-            }
-            else if(cameFrom === 'join'){
-                socket.emit('join_room', userName, roomId);
-            }
-        });
-
-        //update the access code to the the one provided by server
-        //might change in the future when we want to encrypt or hash
-        socket.on('room_created', this.setRoomId);
-
-        socket.on('some_error', (error) => {
-            console.log('got error',error);
-            this.setState({
-                error,
-                redirect:true
-            })
-        })
-
-        socket.on('get_message', function (data) {
-            console.log('Incoming message:', data);
-        });
-
-        socket.on('player_joined_team', (data) => {
-            console.log('data from server : ',data);
-            this.setState({teams: data})
-        })
-
-        
     }
 
 
     onJoinTeam(teamNumber) {
         //notify server
         this.state.socket.emit('join_team',this.state.userName,teamNumber);
-        //listen to server saying 'added this guy' and then 
-
-        //this.state.socket.emit('join_team',this.state.userName, this.state.roomId);
     }
 
     render() {
